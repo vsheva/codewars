57e400be9434ee20164c69fb02f87432312ca99d
vsheva <valera.sheva@gmail.com>
2023-07-18 20:25:36 -0400

    Solution

---
+++ 8kyu/binary-addition.js
@@ -0,0 +1,21 @@
+// ❓ DESCRIPTION:
+//     Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+//
+//     The binary number returned should be a string.
+//
+//     Examples:(Input1, Input2 --> Output (explanation)))
+//
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+
+// ✅ SOLUTION:
+
+//https://www.w3schools.com/jsref/jsref_tostring_number.asp
+
+function addBinary(a,b) {
+return (a+b).toString(2)
+}
+
+console.log(addBinary(1,2))
+console.log(addBinary(9,5))
