9c791c0e2937c115c6595a76607731ab94f1d8b1
vsheva <valera.sheva@gmail.com>
2023-07-19 23:55:16 -0400

    Solution1

--- 7kyu/sorted-yes-no-how.js
+++ 7kyu/sorted-yes-no-how.js
@@ -1,4 +1,7 @@
 // ❓ DESCRIPTION:
+
+//https://www.codewars.com/kata/580a4734d6df748060000045/javascript
+
 //     Complete the method which accepts an array of integers, and returns one of the following:
 //
 //     "yes, ascending" - if the numbers in the array are sorted in an ascending order
@@ -10,10 +13,22 @@
 
 
 function isSortedAndHow(array) {
-    let message = JSON.stringify(array) === JSON.stringify(array.sort((a, b) => a - b)) ? "yes, ascending" :
-        JSON.stringify(array) === JSON.stringify(array.sort((a, b) => b - a)) ? "yes, descending" :
-        "no";
-    return message
+    // let message = JSON.stringify(array) === JSON.stringify(array.sort((a, b) => a - b)) ? "yes, ascending" :
+    //     JSON.stringify(array) === JSON.stringify(array.sort((a, b) => b - a)) ? "yes, descending" :
+    //         "no";
+    // return message
+
+    switch (JSON.stringify(array)) {
+        case JSON.stringify(array.sort((a, b) => a - b)):
+           return "yes, ascending";
+            break;
+        case JSON.stringify(array.sort((a, b) => b - a)):
+           return "yes, descending"
+            break;
+        default:
+           return "no";
+
+    }
 }
 
 console.log(isSortedAndHow([1, 2]))
