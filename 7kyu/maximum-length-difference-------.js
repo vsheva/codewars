da33562c64d6a5ac8b75aefdf61cae8fbd0b14e8
vsheva <valera.sheva@gmail.com>
2023-07-21 00:59:13 -0400

    Solution

--- 7kyu/maximum-length-difference-------.js
+++ 7kyu/maximum-length-difference-------.js
@@ -1,4 +1,6 @@
 // ❓ DESCRIPTION:
+// https://www.codewars.com/kata/5663f5305102699bad000056/javascript
+
 //     You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
 //
 //     Find max(abs(length(x) − length(y)))
