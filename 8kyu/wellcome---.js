d9e410884d30338e9b5412a8e7530e6a85a6c04e
vsheva <valera.sheva@gmail.com>
2023-07-22 02:58:41 -0400

    Solution

--- 8kyu/wellcome---.js
+++ 8kyu/wellcome---.js
@@ -1,4 +1,7 @@
 // ❓ DESCRIPTION:
+
+//https://www.codewars.com/kata/577ff15ad648a14b780000e7/train/javascript
+
 //     Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
 //
 //     The Task
@@ -29,6 +32,7 @@
 // IP_ADDRESS_REQUIRED - no ip address was supplied
 
 // ✅ SOLUTION:
+
  let db={
     english: 'Welcome',
     czech: 'Vitejte',
