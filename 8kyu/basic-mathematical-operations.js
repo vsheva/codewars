94551846be25a265efb879bd172b16e2f0b75c10
vsheva <valera.sheva@gmail.com>
2023-07-26 23:27:36 -0400

    Solution

---
+++ 8kyu/basic-mathematical-operations.js
@@ -0,0 +1,22 @@
+function basicOp(operation, value1, value2)
+{
+    switch(operation) {
+        case "+":
+            return value1 + value2;
+            break;
+        case "-":
+            return value1 - value2;
+            break;
+            return
+        case "*":
+            return value1 * value2;
+            break;
+        case "/":
+            return value1 / value2;
+            break;
+            default:
+                "Введите оператор"
+    }
+    }
+
+    console.log(basicOp('*', 4, 7))
