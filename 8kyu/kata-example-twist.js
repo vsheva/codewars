76dec4b586b6f12261b299827f0a4ca292a53df0
vsheva <valera.sheva@gmail.com>
2023-08-01 10:58:18 -0400

    Solution

---
+++ 8kyu/kata-example-twist.js
@@ -0,0 +1,9 @@
+// ❓ DESCRIPTION:
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+//
+// Add the value "codewars" to the array websites 1,000 times.
+
+// ✅ Solution
+
+let websites = Array(1000).fill("codewars")
+console.log(websites)
