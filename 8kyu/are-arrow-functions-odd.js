e8c24085de30f1a2253afecec0b829062e331c5f
vsheva <valera.sheva@gmail.com>
2023-08-22 11:56:13 -0400

    Solution

---
+++ 8kyu/are-arrow-functions-odd.js
@@ -0,0 +1,12 @@
+// ❓ Time to test your basic knowledge in functions! Return the odds from a list:
+//
+//     [1, 2, 3, 4, 5]  -->  [1, 3, 5]
+//         [2, 4, 6]        -->  []
+
+
+function odds(values){
+
+    return values.filter( el=>el%2===1 );
+}
+
+console.log(odds([1, 2, 3, 4, 5, 6]))
---
+++ 8kyu/exclamation-marks-series-1.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/57fae964d80daa229d000126/javascript
+
+/* ❓ Description:
+    Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+    Examples
+remove("Hi!") == "Hi"
+remove("Hi!!!") == "Hi!!"
+remove("!Hi") == "!Hi"
+remove("!Hi!") == "!Hi"
+remove("Hi! Hi!") == "Hi! Hi"
+remove("Hi") == "Hi"
+*/
+
+// ✅
