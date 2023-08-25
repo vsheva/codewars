c4a4d987f6985981b6b167bbcd7df4a8ee46708b
vsheva <valera.sheva@gmail.com>
2023-08-25 14:45:39 -0400

    Solution

--- 8kyu/define-a-card-suit---.js
+++ 8kyu/define-a-card-suit---.js
@@ -11,6 +11,34 @@
 // ✅ Solution:
 
 // function defineSuit(card) {
+//     switch (card.slice(-1)) {
+//         case '♣':
+//             return "clubs";
+//         case '♦':
+//             return "diamonds";
+//         case '♥':
+//             return "hearts";
+//         case '♠':
+//             return "spades";
+//     }
+// }
+
+function defineSuit(card) {
+
+    console.log(card.slice(-1))
+
+    return {
+         '♣': "clubs",
+         '♦':"diamonds",
+         '♥': "hearts",
+         '♠': "spades",
+    }[card.slice(-1)]
+}
+
+console.log(defineSuit("3♣"))
+
+
+// function defineSuit(card) {
 //     return {
 //         '♣' : 'clubs',
 //         '♦' : 'diamonds',
@@ -18,9 +46,9 @@
 //         '♠' : 'spades'
 //     }[card.substr(-1)]
 
-function defineSuit(card) {
-    if(card.includes('♥')) return 'hearts'
-    if(card.includes('♦')) return 'diamonds'
-    if(card.includes('♣')) return 'clubs'
-    if(card.includes('♠')) return 'spades'
-}
+// function defineSuit(card) {
+//     if(card.includes('♥')) return 'hearts'
+//     if(card.includes('♦')) return 'diamonds'
+//     if(card.includes('♣')) return 'clubs'
+//     if(card.includes('♠')) return 'spades'
+// }
