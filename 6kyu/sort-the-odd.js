d4233df3d0738dc381e0bb2381c34d0525a58bb6
vsheva <valera.sheva@gmail.com>
2023-10-22 12:47:20 -0400

    Solution

--- 6kyu/sort-the-odd.js
+++ 6kyu/sort-the-odd.js
@@ -8,8 +8,10 @@
 
 
 function sortArray(array) {
-    const odds = array.filter((num) => num % 2 === 1).sort((a,b)=>a-b);
-    console.log(odds)
+    const odds = array.filter((num) => num % 2===1).sort((a,b)=>a-b);
+    //console.log(odds)   // [ 1, 3, 5 ]
+
+    return array.map((x)=> x%2===1 ? odds.shift() : x)
 }
 
 console.log(sortArray([5, 3, 2, 8, 1, 4]))
