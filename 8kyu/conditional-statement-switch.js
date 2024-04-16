a01a0b47151274188d6847d84b29f2bd708239bc
vsheva <valera.sheva@gmail.com>
2024-04-15 22:17:57 -0400

    refactoring

--- 8kyu/conditional-statement-switch.js
+++ 8kyu/conditional-statement-switch.js
@@ -1,28 +1,16 @@
-function howManydays(month){
+function howManydays(month) {
     let days;
-    switch (month){
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
-            days=31;
+    switch (month) {
+        case 2:
+            return days = 28;
             break;
         case 4:
         case 6:
         case 9:
         case 11:
-            days=30;
-            break;
-        case 2:
-            days=28;
-            break;
-        default:"Enter a correct month number"
-
+            return days = 30;
     }
-    return days;
+    return days = 31
 }
 
 console.log(howManydays(1))
