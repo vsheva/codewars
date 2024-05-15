6e3df5c622ef2ec1a7394ed3451a07dd7194caa7
vsheva <valera.sheva@gmail.com>
2024-05-14 23:05:49 -0400

    fix

---
+++ 8kyu/color-ghost.js
@@ -0,0 +1,7 @@
+let color = ['white', 'yellow', 'purple', 'red'];
+
+var Ghost = function() {
+    // your code goes here
+
+    this.color = ['white', 'yellow', 'purple', 'red'][Math.floor(Math.random()*4)]
+};
