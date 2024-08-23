0459e4dfbc42858cd54c359b6b8eba1237ce1b02
vsheva <valera.sheva@gmail.com>
2024-08-23 00:03:21 -0400

    done

--- 8kyu/is-it-number.js
+++ 8kyu/is-it-number.js
@@ -1,11 +1,9 @@
 //https://www.codewars.com/kata/57126304cdbf63c6770012bd/train/javascript
 
 function isDigit(s) {
-    //return isNaN(+s) ? false : true
-    console.log(parseFloat(s))
     return s== parseFloat(s)
 }
 
-console.log(isDigit("  3.14  "))
+console.log(isDigit("0.0314E+2 "))
 
 
