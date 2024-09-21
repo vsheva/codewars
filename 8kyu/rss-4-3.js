aa8aeab43f9d5af0e9889e4302be079c5d18d2f9
vsheva <valera.sheva@gmail.com>
2024-09-21 15:59:30 -0400

    fix

--- 8kyu/rss-4-3.js
+++ 8kyu/rss-4-3.js
@@ -7,14 +7,6 @@
 // 5 9 12 14 15
 
 
-//Task1
-// 1
-// 23
-// 456
-// 78910
-// 1112131415
-
-
 // let result=""
 // let current=1
 // let rows =5
@@ -30,22 +22,56 @@
 //  console.log(result)
 
 
+//Task1
+// 1
+// 23
+// 456
+// 78910
+// 1112131415
 
-let result=""
-let rows =5
+// let result=""
+// let rows =5
+// let start = 1
+//
+// for (let i = 1; i <= rows; i++) {
+//
+//     for (let col = 1; col<=i; col++) {
+//         result+=start;
+//         start++
+//         //start = start + (rows -col)
+//     }
+//     result =result +"\n"
+// }
+// console.log(result)
+
+
+// 1
+// 2 6
+// 3 7 10
+// 4 8 11 13
+// 5 9 12 14 15
+
+let result = ""
+let rows = 5
 let start = 1
 
 for (let i = 1; i <= rows; i++) {
-
-    for (let col = 1; col<=i; col++) {
-        result+=start;
-        start++
-        //start = start + (rows -col)
+    start = i;// создаем начало для каждой строки
+    for (let col = 1; col <= i; col++) {
+        result += start;
+        //start++
+        start = start + (rows - col)
     }
-    result =result +"\n"
+    // между первой и второй колонкой разделение 4 (это на 1 меньше количеста rows); м
+    // между второй и третьей - шаг 3 (это на 2 меньше количеста rows);
+    //между третьей и четвертой - шаг 2 (это на 3 меньше количеста rows)
+    // между четвертой и пятой - шаг 1 (это на 4 меньше количеста rows);
+    //---------------------------------------------------------------
+    //вывод -  каждый шаг меньше на  внутренний индекс col
+    // на каждом шаге  (rows-col)
+    result = result + "\n"
 }
 console.log(result)
-
 
 
 // Определяем количество строк в треугольнике
@@ -100,8 +126,8 @@ console.log(result)
 // };
 // console.log(sum(1))
 
-
-let sum = function(a, minus = function(){return 7-2}) {
-    return a + minus();
-};
-console.log(sum(1))
+//
+// let sum = function(a, minus = function(){return 7-2}) {
+//     return a + minus();
+// };
+// console.log(sum(1))
