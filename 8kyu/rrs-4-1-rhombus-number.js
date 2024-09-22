23fb67a8af619ee1ea7d946dec7c21d4182c1052
vsheva <valera.sheva@gmail.com>
2024-09-21 21:17:47 -0400

    rhombous is done

--- 8kyu/rrs-4-1-rhombus-number.js
+++ 8kyu/rrs-4-1-rhombus-number.js
@@ -1,32 +1,8 @@
-//Task1
-// 1
-// 23
-// 456
-// 78910
-// 1112131415
-
-// let result=""
-// // let rows =5
-// // let start = 1
-// //
-// // for (let i = 1; i <= rows; i++) {
-// //
-// //     for (let col = 1; col<=i; col++) {
-// //         result+=start;
-// //         start++
-// //
-// //     }
-// //     result =result +"\n"
-// // }
-// // console.log(result)
-
-
-
 let result = "";
-
-for (let str = 10; str >= 1; str--) {
+//верх
+for (let str = 10; str > 1; str--) {
     for (let space = 1; space < str; space++) {
-        result += "_"
+        result += " " //space "_"
     }
 
     let start = 1
@@ -36,113 +12,49 @@ for (let str = 10; str >= 1; str--) {
     }
 
     let start2 = 1
+    let row = [];
     for (let star = str; star < 10; star++) {
-
-        result += start2;
+        row.push(start2)
+        //result += start2;
         start2++;
     }
-
-    result =result +"\n"
+    result += row.reverse().join('') + "\n"
 }
 
 
+//середина
+for (let i = 1; i <= 10; i++) {
+    if (i === 10) {
+        result += 0
+        break;
+    }
+    result += i;
+}
+
+let arr = []
+for (let i = 1; i <= 9; i++) {
+    arr.push(i)
+    //result += i;
+}
+result += arr.reverse().join('') + "\n";
+
+
+//низ
 for (let str = 9; str >= 1; str--) {
     for (let space = str; space <= 9; space++) {
-        result += "_"
+        result += " " //space "_"
     }
 
-    for (let star = 1; star <= str; star++) {
-        result += star
+    for (let m = 1; m <= str; m++) {
+        result += m;
     }
 
-    for (let star = 1; star < str; star++) {
-        result += star
+    let row = []
+    for (let n = 1; n < str; n++) {
+        row.push(n)
+        //result += n
     }
-
-    result = result + "\n"
+    result += row.reverse().join('') + "\n"
 }
-
 
 console.log(result)
-
-
-
-
-
-
-
-/*
-
-let result = "";
-
-for (let str = 10; str >= 1; str--) {
-    for (let space = 1; space < str; space++) {
-        result += "_"
-    }
-
-    let start = 1
-    for (let i = str; i <= 10; i++) {
-        result += start;
-        start++;
-    }
-
-    let start2 = 1
-    let currentString=''
-    for (let star = str; star < 10; star++) {
-        //currentString=`${start}${currentString}`
-        result += start2;
-        start2++;
-    }
-
-    result =result +"\n"
-}
-
-
-for (let str = 9; str >= 1; str--) {
-    for (let space = str; space <= 9; space++) {
-        result += "_"
-    }
-
-    for (let star = 1; star <= str; star++) {
-        result += "*"
-    }
-
-    for (let star = 1; star < str; star++) {
-        result += "*"
-    }
-
-    result = result + "\n"
-}
-
-
-console.log(result)
-*/
-
-
-
-
-
-
-
-
-
-
-
-// let result = "";
-// let rows = 5;
-// let start = 1;
-//
-// for (let i = 1; i <= rows; i++) {
-//     let currentLine = ""; // Строка для текущей строки
-//
-//     // Сначала формируем строку
-//     for (let col = 1; col <= i; col++) {
-//         currentLine = ` ${start} ${currentLine}`; // Добавляем число в начало строки
-//         console.log("current line: " + currentLine)
-//         start++;
-//     }
-//
-//     result += currentLine + "\n"; // Добавляем текущую строку в результат
-// }
-//
-// console.log(result);
--- 8kyu/rss-3.js
+++ 8kyu/rss-3.js
@@ -5,7 +5,7 @@
 // 1
 
 
-let result =""
+/*let result =""
 
 for (let i = 5; i >=1; i--) {
     for (let j = 1; j<=i; j++) {
@@ -13,7 +13,7 @@ for (let i = 5; i >=1; i--) {
     }
     result +="\n"
 }
-console.log(result)
+console.log(result)*/
 
 //1 2 3 4 5
 //1234
@@ -31,6 +31,36 @@ console.log(result)
 // }
 //
 // console.log(reverse([1,2,3]))
+
+
+
+let result = "";
+
+for (let str = 10; str > 1; str--) {
+    for (let space = 1; space < str; space++) {
+        result += "_";
+    }
+
+    let start = 1;
+    for (let i = str; i <= 10; i++) {
+        result += start;
+        start++;
+    }
+
+    let start2 = 1;
+    let row = [];
+    for (let star = str; star < 10; star++) {
+        row.push(start2);
+        start2++;
+    }
+
+    // Добавляем строку только если это не последняя
+
+        result += row.reverse().join('') + "\n";
+
+}
+
+console.log(result);
 
 
 
