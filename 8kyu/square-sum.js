52c42c1c93001385e60b2fd1f5d11361ed714b60
vsheva <valera.sheva@gmail.com>
2024-10-08 00:30:54 -0400

    done

--- 8kyu/square-sum.js
+++ 8kyu/square-sum.js
@@ -26,19 +26,35 @@
 // //console.log(obj);
 
 
+/*
 const obj = {};
+
 let carModel = 'car'; //через переменную
+let name ="volvo"
 let num = 2
-obj[carModel] = 'volvo';
+//obj.carModel="volvo"; // carModel: 'volvo'
+obj[carModel] = name; //ПЕРЕДАЕМ ПЕРЕМЕННУЮ В КЛЮЧ !!!!        carModel("car"):name("volvo")
+obj[carModel] = 5;
 obj['carModel'] = num;
 console.log(obj); //{ car: 'volvo', carModel: 2 }
+*/
 
 
-let name1= "Anna"
-let age= 17
-const myPerson = {
-  [name1]: name1,
-  age: age
+// let name1= "Anna"
+// let age= 17
+// const myPerson = {
+//   [name1]: name1,
+//   [age]: age
+// }
+//
+// console.log(myPerson)
+
+
+let obj={
+  name1: "John",
+  age: 17
 }
 
-console.log(myPerson)
+for (let key in obj) {
+  console.log(obj[key])
+}
