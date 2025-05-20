37a1bb9ab70d70b55a733aeb642e96c36a862566
vsheva <shev1181@gmail.com>
2025-05-19 23:38:18 -0400

    solve

--- 6kyu/multiples-of-3-or-5.js
+++ 6kyu/multiples-of-3-or-5.js
@@ -1,6 +1,16 @@
 //https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
 
 
-function solution(number){
-  
+function solution(number) {
+    let sum = 0;
+
+    for (let i = 0; i < number; i++) {
+
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i
+        }
+    }
+    return sum
 }
+
+console.log(solution(10))
